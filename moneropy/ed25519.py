85cf0f572ed6d617f8fe60db01410bd91a9ad803
Mike C <bigreddmachine@users.noreply.github.com>
2016-11-02 13:19:01 -0600

    Add Shen Noether's additions to ed25519.py
    Source:
    https://github.com/monero-project/mininero/blob/master/ed25519.py
    
    (Included under BSD 3-Clause License)

--- moneropy/ed25519.py
+++ moneropy/ed25519.py
@@ -105,3 +105,93 @@ def checkvalid(s,m,pk):
   h = Hint(encodepoint(R) + pk + m)
   if scalarmult(B,S) != edwards(R,scalarmult(A,h)):
     raise Exception("signature does not pass verification")
+
+# ----------
+# The following additions are from Shen Noether's ed25519.py in MiniNero.
+# Source: https://github.com/monero-project/mininero/blob/master/ed25519.py
+
+# Copyright (c) 2014-2016, The Monero Project
+#
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without modification, are
+# permitted provided that the following conditions are met:
+#
+# 1. Redistributions of source code must retain the above copyright notice, this list of
+#    conditions and the following disclaimer.
+#
+# 2. Redistributions in binary form must reproduce the above copyright notice, this list
+#    of conditions and the following disclaimer in the documentation and/or other
+#    materials provided with the distribution.
+#
+# 3. Neither the name of the copyright holder nor the names of its contributors may be
+#    used to endorse or promote products derived from this software without specific
+#    prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY
+# EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+# MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL
+# THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+# SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+# PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+# INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+# STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF
+# THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+def radix255(x):
+  x = x % q
+  if x + x > q: x -= q
+  x = [x,0,0,0,0,0,0,0,0,0]
+  bits = [26,25,26,25,26,25,26,25,26,25]
+  for i in range(9):
+    carry = (x[i] + 2**(bits[i]-1)) / 2**bits[i]
+    x[i] -= carry * 2**bits[i]
+    x[i + 1] += carry
+  result = ""
+  for i in range(9):
+    result = result+str(x[i])+","
+  result = result+str(x[9])
+  return result
+
+def theD():
+  return d
+
+def computeA():
+  return 2 *((1 - d) % q) * inv((1 + d) % q) % q
+
+def sqroot(xx):
+  x = expmod(xx,(q+3)/8,q)
+  if (x*x - xx) % q != 0:
+    x = (x*I) % q
+  if (x*x - xx) % q != 0:
+    print("no square root!")
+  return x
+
+def edwards_Minus(P, Q): #added
+  x1 = P[0]
+  y1 = P[1]
+  x2 = (-1 * Q[0]) % q
+  y2 = Q[1]
+  x3 = (x1*y2+x2*y1) * inv(1+d*x1*x2*y1*y2)
+  y3 = (y1*y2+x1*x2) * inv(1-d*x1*x2*y1*y2)
+  return [x3 % q,y3 % q]
+
+#added scalarmultbase
+def scalarmultbase(e):
+  if e == 0: return [0,1]
+  Q = scalarmult(B,e/2)
+  Q = edwards(Q,Q)
+  if e & 1: Q = edwards(Q,B)
+  return Q
+
+def decodepointcheck(s):
+  y = sum(2**i * bit(s,i) for i in range(0,b-1))
+  x = xrecover(y)
+  if x & 1 != bit(s,b-1): x = q-x
+  P = [x,y]
+  #print("actually checking if it's on curve!")
+  if not isoncurve(P):
+    #print("not on curve")
+    quit()
+    raise Exception("decoding point that is not on curve")
+  return P
