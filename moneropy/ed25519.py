90e401aded1a133109608646d22faf682f88e49f
Mike C <bigreddmachine@users.noreply.github.com>
2016-11-02 14:24:41 -0600

    Made ed25519.py more readable and pythonic
    - Improved spacing and made more consistent and pythonic
    - Made operations more pythonic and order of operations more explicit

--- moneropy/ed25519.py
+++ moneropy/ed25519.py
@@ -10,74 +10,74 @@ l = 2**252 + 27742317777372353535851937790883648493
 def H(m):
   return hashlib.sha512(m).digest()
 
-def expmod(b,e,m):
+def expmod(b, e, m):
   if e == 0: return 1
-  t = expmod(b,e//2,m)**2 % m
+  t = expmod(b, e//2, m)**2 % m
   if e & 1: t = (t*b) % m
   return t
 
 def inv(x):
-  return expmod(x,q-2,q)
+  return expmod(x, q-2, q)
 
 d = -121665 * inv(121666)
-I = expmod(2,(q-1)//4,q)
+I = expmod(2, (q-1)//4, q)
 
 def xrecover(y):
   xx = (y*y-1) * inv(d*y*y+1)
-  x = expmod(xx,(q+3)//8,q)
+  x = expmod(xx, (q+3)//8, q)
   if (x*x - xx) % q != 0: x = (x*I) % q
   if x % 2 != 0: x = q-x
   return x
 
 By = 4 * inv(5)
 Bx = xrecover(By)
-B = [Bx % q,By % q]
+B = [Bx%q, By%q]
 
-def edwards(P,Q):
+def edwards(P, Q):
   x1 = P[0]
   y1 = P[1]
   x2 = Q[0]
   y2 = Q[1]
   x3 = (x1*y2+x2*y1) * inv(1+d*x1*x2*y1*y2)
   y3 = (y1*y2+x1*x2) * inv(1-d*x1*x2*y1*y2)
-  return [x3 % q,y3 % q]
+  return [x3%q, y3%q]
 
-def scalarmult(P,e):
-  if e == 0: return [0,1]
-  Q = scalarmult(P,e//2)
-  Q = edwards(Q,Q)
-  if e & 1: Q = edwards(Q,P)
+def scalarmult(P, e):
+  if e == 0: return [0, 1]
+  Q = scalarmult(P, e//2)
+  Q = edwards(Q, Q)
+  if e & 1: Q = edwards(Q, P)
   return Q
 
 def encodeint(y):
   bits = [(y >> i) & 1 for i in range(b)]
-  return ''.join([chr(sum([bits[i * 8 + j] << j for j in range(8)])) for i in range(b//8)])
+  return ''.join([chr(sum([bits[i*8 + j] << j for j in range(8)])) for i in range(b//8)])
 
 def encodepoint(P):
   x = P[0]
   y = P[1]
-  bits = [(y >> i) & 1 for i in range(b - 1)] + [x & 1]
+  bits = [(y >> i) & 1 for i in range(b-1)] + [x & 1]
   return ''.join([chr(sum([bits[i * 8 + j] << j for j in range(8)])) for i in range(b//8)])
 
-def bit(h,i):
+def bit(h, i):
   return (ord(h[i//8]) >> (i%8)) & 1
 
 def publickey(sk):
   h = H(sk)
-  a = 2**(b-2) + sum(2**i * bit(h,i) for i in range(3,b-2))
-  A = scalarmult(B,a)
+  a = 2**(b-2) + sum(2**i * bit(h, i) for i in range(3, b-2))
+  A = scalarmult(B, a)
   return encodepoint(A)
 
 def Hint(m):
   h = H(m)
-  return sum(2**i * bit(h,i) for i in range(2*b))
+  return sum(2**i * bit(h, i) for i in range(2*b))
 
-def signature(m,sk,pk):
+def signature(m, sk, pk):
   h = H(sk)
-  a = 2**(b-2) + sum(2**i * bit(h,i) for i in range(3,b-2))
-  r = Hint(''.join([h[i] for i in range(b//8,b//4)]) + m)
-  R = scalarmult(B,r)
-  S = (r + Hint(encodepoint(R) + pk + m) * a) % l
+  a = 2**(b-2) + sum(2**i * bit(h, i) for i in range(3, b-2))
+  r = Hint(''.join([h[i] for i in range(b//8, b//4)]) + m)
+  R = scalarmult(B, r)
+  S = (r + Hint(encodepoint(R)+pk+m) * a) % l
   return encodepoint(R) + encodeint(S)
 
 def isoncurve(P):
@@ -86,24 +86,24 @@ def isoncurve(P):
   return (-x*x + y*y - 1 - d*x*x*y*y) % q == 0
 
 def decodeint(s):
-  return sum(2**i * bit(s,i) for i in range(0,b))
+  return sum(2**i * bit(s, i) for i in range(0, b))
 
 def decodepoint(s):
-  y = sum(2**i * bit(s,i) for i in range(0,b-1))
+  y = sum(2**i * bit(s, i) for i in range(0, b-1))
   x = xrecover(y)
-  if x & 1 != bit(s,b-1): x = q-x
-  P = [x,y]
+  if x & 1 != bit(s, b-1): x = q - x
+  P = [x, y]
   if not isoncurve(P): raise Exception("decoding point that is not on curve")
   return P
 
-def checkvalid(s,m,pk):
+def checkvalid(s, m, pk):
   if len(s) != b//4: raise Exception("signature length is wrong")
   if len(pk) != b//8: raise Exception("public-key length is wrong")
   R = decodepoint(s[0:b//8])
   A = decodepoint(pk)
   S = decodeint(s[b//8:b//4])
   h = Hint(encodepoint(R) + pk + m)
-  if scalarmult(B,S) != edwards(R,scalarmult(A,h)):
+  if scalarmult(B, S) != edwards(R, scalarmult(A, h)):
     raise Exception("signature does not pass verification")
 
 # ----------
@@ -141,26 +141,23 @@ def checkvalid(s,m,pk):
 def radix255(x):
   x = x % q
   if x + x > q: x -= q
-  x = [x,0,0,0,0,0,0,0,0,0]
-  bits = [26,25,26,25,26,25,26,25,26,25]
+  x = [x, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+  bits = [26, 25, 26, 25, 26, 25, 26, 25, 26, 25]
   for i in range(9):
-    carry = (x[i] + 2**(bits[i]-1)) // 2**bits[i]
+    carry = (x[i] + 2**(bits[i]-1)) // (2**bits[i])
     x[i] -= carry * 2**bits[i]
     x[i + 1] += carry
-  result = ""
-  for i in range(9):
-    result = result+str(x[i])+","
-  result = result+str(x[9])
+  result = ",".join(str(xi) for xi in x)
   return result
 
 def theD():
   return d
 
 def computeA():
-  return 2 *((1 - d) % q) * inv((1 + d) % q) % q
+  return 2 * ((1-d)%q) * inv((1+d)%q) % q
 
 def sqroot(xx):
-  x = expmod(xx,(q+3)//8,q)
+  x = expmod(xx, (q+3)//8, q)
   if (x*x - xx) % q != 0:
     x = (x*I) % q
   if (x*x - xx) % q != 0:
@@ -170,24 +167,24 @@ def sqroot(xx):
 def edwards_Minus(P, Q):
   x1 = P[0]
   y1 = P[1]
-  x2 = (-1 * Q[0]) % q
+  x2 = (-Q[0]) % q
   y2 = Q[1]
   x3 = (x1*y2+x2*y1) * inv(1+d*x1*x2*y1*y2)
   y3 = (y1*y2+x1*x2) * inv(1-d*x1*x2*y1*y2)
-  return [x3 % q,y3 % q]
+  return [x3%q, y3%q]
 
 def scalarmultbase(e):
-  if e == 0: return [0,1]
-  Q = scalarmult(B,e//2)
-  Q = edwards(Q,Q)
-  if e & 1: Q = edwards(Q,B)
+  if e == 0: return [0, 1]
+  Q = scalarmult(B, e//2)
+  Q = edwards(Q, Q)
+  if e & 1: Q = edwards(Q, B)
   return Q
 
 def decodepointcheck(s):
-  y = sum(2**i * bit(s,i) for i in range(0,b-1))
+  y = sum(2**i * bit(s, i) for i in range(0, b-1))
   x = xrecover(y)
-  if x & 1 != bit(s,b-1): x = q-x
-  P = [x,y]
+  if x & 1 != bit(s, b-1): x = q - x
+  P = [x, y]
   if not isoncurve(P):
     quit()
     raise Exception("decoding point that is not on curve")
