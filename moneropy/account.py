7265e16ce1038bb23e6a4ad60fd086f04e44d71b
Mike C <bigreddmachine@users.noreply.github.com>
2016-11-04 01:24:32 -0600

    Documenting account.py functions

--- moneropy/account.py
+++ moneropy/account.py
@@ -16,11 +16,26 @@ def get_view_key(sk):
     return _cn.sc_reduce(_cn.cn_fast_hash(sk))
 
 def encode_addr(version, publicSpendKey, publicViewKey):
+    '''Given address version and public spend and view keys, derive address.'''
     data = version + publicSpendKey + publicViewKey
     checksum = _cn.cn_fast_hash(data)
     return _b58.b58encode(data + checksum[0:8])
 
-def account_from_secret_key(sk, acct_type='simplewallet'):
+def account_from_spend_key(sk, acct_type='simplewallet'):
+    '''Given a private spend key, derive private view key and address.
+
+    Inputs:
+      - sk (str) -- private spend key
+      - acct_type (str, optional) -- 'simplewallet' (default) OR 'mymonero'
+
+    Example:
+      sk, vk, addr = account_from_seed(seed)
+
+    Outputs:
+      - sk (str) -- private spend key
+      - vk (str) -- private view key
+      - addr (str) -- Monero address
+    '''
     if acct_type == 'mymonero':
         vk = get_view_key(sk)
         sk = _cn.sc_reduce(sk)
@@ -38,5 +53,19 @@ def account_from_secret_key(sk, acct_type='simplewallet'):
     return sk, vk, addr
 
 def account_from_seed(seed, acct_type='simplewallet'):
+    '''Given a wallet seed, derive private spend and view keys and address.
+
+    Inputs:
+      - seed (list) -- list of seed words from which to derive account info
+      - acct_type (str, optional) -- 'simplewallet' (default) OR 'mymonero'
+
+    Example:
+      sk, vk, addr = account_from_seed(seed)
+
+    Outputs:
+      - sk (str) -- private spend key
+      - vk (str) -- private view key
+      - addr (str) -- Monero address
+    '''
     sk = _mn.mn_decode(seed)
-    return account_from_secret_key(sk, acct_type)
+    return account_from_spend_key(sk, acct_type)
